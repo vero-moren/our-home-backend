33cc6974247be313601e06dcac947db51457993f
vero-moren <mmmonstereer@gmail.com>
2026-07-21 14:55:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1243,6 +1243,7 @@ app.post("/study/stream", async (req, res) => {
   res.setHeader("Content-Type", "text/event-stream; charset=utf-8");
   res.setHeader("Cache-Control", "no-cache, no-transform");
   res.setHeader("X-Accel-Buffering", "no");
+  res.setHeader("Content-Encoding", "identity");
   res.flushHeaders?.();
 
   const controller = new AbortController();
